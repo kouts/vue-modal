7b2c2aa10204442e4f98791db3075e1475e7ed00
kouts <koutsaftakis@gmail.com>
2019-11-01 18:40:45 +0200

    Implemented workarounds for IE11 compatibility

---
+++ demo/assets/js/7.c4c2a6d8.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{145:function(n,w,o){}}]);
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,28 @@
 module.exports = {
   dest: 'demo',
   base: '/vue-modal/demo/',
+  // Use this base to test localy
+  // base: '/demo/',
   title: 'vue-modal',
   description: 'A modal plugin for Vue',
   themeConfig: {
     nav: [{ text: 'Guide', link: '/' }],
     sidebar: [['/', 'Guide']]
-  }
+  },
+  head: [
+    [
+      'script',
+      {src: 'https://polyfill.io/v3/polyfill.min.js?features=NodeList.prototype.forEach'}],
+  ],  
+  chainWebpack: (config, isServer) => {
+    const compileRule = config.module.rule('compile');
+    compileRule.test(/\.js$/).include.add(/@vuepress/).add(/.temp/).end().use('babel-loader').loader('babel-loader').options({
+      presets: [
+        ['@babel/preset-env', {
+          useBuiltIns: 'usage'
+        }]
+      ],
+      plugins: ['@babel/plugin-syntax-dynamic-import']
+    });
+  }  
 };
