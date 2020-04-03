802ef21fc62f24fed4db76a7c561085240b8c297
E6440\Kouts <koutsaftakis@gmail.com>
2020-04-03 22:35:11 +0300

    Removed workaround code for IE11, added polyfills for Array.from, Promise and Object.assign

---
+++ demo/assets/js/11.37f2485b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{152:function(n,w,o){}}]);
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,17 +18,19 @@ module.exports = {
   head: [
     [
       'script',
-      {src: 'https://polyfill.io/v3/polyfill.min.js?features=NodeList.prototype.forEach'}],
-  ],  
-  chainWebpack: (config, isServer) => {
-    const compileRule = config.module.rule('compile');
-    compileRule.test(/\.js$/).include.add(/@vuepress/).add(/.temp/).end().use('babel-loader').loader('babel-loader').options({
-      presets: [
-        ['@babel/preset-env', {
-          useBuiltIns: 'usage'
-        }]
-      ],
-      plugins: ['@babel/plugin-syntax-dynamic-import']
-    });
-  }  
+      {src: 'https://polyfill.io/v3/polyfill.min.js?features=Array.from'}
+    ],    
+    [
+      'script',
+      {src: 'https://polyfill.io/v3/polyfill.min.js?features=Promise'}
+    ],
+    [
+      'script',
+      {src: 'https://polyfill.io/v3/polyfill.min.js?features=NodeList.prototype.forEach'}
+    ],
+    [
+      'script',
+      {src: 'https://polyfill.io/v3/polyfill.min.js?features=Object.assign'}
+    ]
+  ]
 };
