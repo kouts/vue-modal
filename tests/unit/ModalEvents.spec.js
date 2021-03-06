2581b8263211eb21d2bc05769e54d940e4372d30
kouts <koutsaftakis@gmail.com>
2021-03-06 12:44:59 +0200

    Updated tests

--- tests/unit/ModalEvents.spec.js
+++ tests/unit/ModalEvents.spec.js
@@ -1,6 +1,6 @@
 import { mount } from '@vue/test-utils';
 import '@testing-library/jest-dom';
-import { waitNT, waitRAF } from '../utils';
+import { waitNT, waitRAF, sleep } from '../utils';
 import Modal from '@/Modal.vue';
 
 describe('Modal events', () => {
@@ -23,15 +23,11 @@ describe('Modal events', () => {
     wrapper.setProps({ basedOn: true });
     await waitNT(wrapper.vm);
     await waitRAF();
-    await waitNT(wrapper.vm);
-    await waitRAF();
     expect(wrapper.emitted('before-open')).toBeTruthy();
   });
 
   it('emits an opening event', async () => {
     wrapper.setProps({ basedOn: true });
-    await waitNT(wrapper.vm);
-    await waitRAF();
     await waitNT(wrapper.vm);
     await waitRAF();
     expect(wrapper.emitted('opening')).toBeTruthy();
@@ -41,15 +37,12 @@ describe('Modal events', () => {
     wrapper.setProps({ basedOn: true });
     await waitNT(wrapper.vm);
     await waitRAF();
-    await waitNT(wrapper.vm);
-    await waitRAF();
+    await sleep(200);
     expect(wrapper.emitted('after-open')).toBeTruthy();
   });
 
   it('emits a before-close event', async () => {
     wrapper.setProps({ basedOn: false });
-    await waitNT(wrapper.vm);
-    await waitRAF();
     await waitNT(wrapper.vm);
     await waitRAF();
     expect(wrapper.emitted('before-close')).toBeTruthy();
@@ -59,15 +52,11 @@ describe('Modal events', () => {
     wrapper.setProps({ basedOn: false });
     await waitNT(wrapper.vm);
     await waitRAF();
-    await waitNT(wrapper.vm);
-    await waitRAF();
     expect(wrapper.emitted('closing')).toBeTruthy();
   });
 
   it('emits an after-close event', async () => {
     wrapper.setProps({ basedOn: false });
-    await waitNT(wrapper.vm);
-    await waitRAF();
     await waitNT(wrapper.vm);
     await waitRAF();
     expect(wrapper.emitted('after-close')).toBeTruthy();
--- tests/utils.js
+++ tests/utils.js
@@ -6,3 +6,4 @@ export const createContainer = (tag = 'div') => {
 
 export const waitNT = ctx => new Promise(resolve => ctx.$nextTick(resolve));
 export const waitRAF = () => new Promise(resolve => requestAnimationFrame(resolve));
+export const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
