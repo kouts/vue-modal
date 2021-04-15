91868c869a8b8a6bbc0a6bb4d8850a44757d6396
Kouts <koutsaftakis@gmail.com>
2021-04-15 22:30:07 +0300

    Applied formatting

--- docs/.vuepress/components/Intro.vue
+++ docs/.vuepress/components/Intro.vue
@@ -2,10 +2,14 @@
   <div>
     <div class="row mb-3">
       <div class="col">
-        <button type="button" class="btn btn-primary" @click="showfirstModal=true">
+        <button type="button" class="btn btn-primary" @click="showfirstModal = true">
           Open a modal
         </button>
-        <button type="button" class="btn btn-primary" @click="showfirstModal=true; showSecondModal=true;">
+        <button
+          type="button"
+          class="btn btn-primary"
+          @click="showfirstModal = true; showSecondModal = true;"
+        >
           Show all modals
         </button>
       </div>
@@ -16,20 +20,20 @@
         <form novalidate>
           <div class="form-group">
             <label for="inputEmail4">Email</label>
-            <input id="inputEmail4" type="email" class="form-control" placeholder="Email">
+            <input id="inputEmail4" type="email" class="form-control" placeholder="Email" />
           </div>
           <div class="form-group">
             <label for="inputAddress">Address</label>
-            <input id="inputAddress" type="text" class="form-control" placeholder="1234 Main St">
+            <input id="inputAddress" type="text" class="form-control" placeholder="1234 Main St" />
           </div>
           <div class="form-group">
             <label for="inputAddress2">Address 2</label>
-            <input id="inputAddress2" type="text" class="form-control" placeholder="Apartment, studio, or floor">
+            <input id="inputAddress2" type="text" class="form-control" placeholder="Apartment, studio, or floor" />
           </div>
           <div class="form-row">
             <div class="form-group col-md-6">
               <label for="inputCity">City</label>
-              <input id="inputCity" type="text" class="form-control">
+              <input id="inputCity" type="text" class="form-control" />
             </div>
             <div class="form-group col-md-4">
               <label for="inputState">State</label>
@@ -40,26 +44,26 @@
             </div>
             <div class="form-group col-md-2">
               <label for="inputZip">Zip</label>
-              <input id="inputZip" type="text" class="form-control">
+              <input id="inputZip" type="text" class="form-control" />
             </div>
           </div>
           <div class="form-group">
             <div class="form-check">
-              <input id="gridCheck" class="form-check-input" type="checkbox">
+              <input id="gridCheck" class="form-check-input" type="checkbox" />
               <label class="form-check-label" for="gridCheck">
                 Check me out
               </label>
             </div>
           </div>
-          <hr class="full-hr">
+          <hr class="full-hr" />
           <div class="row">
             <div class="col-sm-12">
               <div class="float-left">
-                <button class="btn btn-warning" type="button" @click="showSecondModal=true">Open second modal</button>
+                <button class="btn btn-warning" type="button" @click="showSecondModal = true">Open second modal</button>
               </div>
               <div class="float-right">
-                <button class="btn btn-primary" type="button" @click="showfirstModal=false">Ok</button>
-                <button class="btn btn-secondary ml-2" type="button" @click="showfirstModal=false">Cancel</button>
+                <button class="btn btn-primary" type="button" @click="showfirstModal = false">Ok</button>
+                <button class="btn btn-secondary ml-2" type="button" @click="showfirstModal = false">Cancel</button>
               </div>
             </div>
           </div>
@@ -70,18 +74,18 @@
         <form novalidate>
           <div class="form-group">
             <label for="formField1">Field 1</label>
-            <input id="formField1" type="text" class="form-control" placeholder="">
+            <input id="formField1" type="text" class="form-control" placeholder="" />
           </div>
           <div class="form-group">
             <label for="formField2">Field 2</label>
-            <input id="formField2" type="text" class="form-control" placeholder="">
+            <input id="formField2" type="text" class="form-control" placeholder="" />
           </div>
-          <hr class="full-hr">
+          <hr class="full-hr" />
           <div class="row">
             <div class="col-sm-12">
               <div class="float-right">
-                <button class="btn btn-primary" type="button" @click="showSecondModal=false">Ok</button>
-                <button class="btn btn-secondary ml-2" type="button" @click="showSecondModal=false">Cancel</button>
+                <button class="btn btn-primary" type="button" @click="showSecondModal = false">Ok</button>
+                <button class="btn btn-secondary ml-2" type="button" @click="showSecondModal = false">Cancel</button>
               </div>
             </div>
           </div>
@@ -93,8 +97,7 @@
 
 <script>
 export default {
-  name: 'Intro',
-  data: function () {
+  data () {
     return {
       showfirstModal: false,
       showSecondModal: false
