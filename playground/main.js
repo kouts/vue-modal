29663f865cb1d4696bb44fc440849611e24db78f
Kouts <koutsaftakis@gmail.com>
2021-11-20 20:54:42 +0200

    docs: added playground examples

--- playground/main.js
+++ playground/main.js
@@ -3,9 +3,11 @@ import { router } from './router'
 import { store } from './store'
 import Default from './layouts/default/Default.vue'
 import App from './App.vue'
+import Modal from '@/Modal.vue'
 
 const app = createApp(App)
 app.component('LayoutDefault', Default)
+app.component('Modal', Modal)
 
 app.use(store)
 app.use(router)
--- playground/views/Example2.vue
+++ playground/views/Example2.vue
@@ -1,7 +1,29 @@
 <template>
   <div class="container">
-    <div class="mb-3">
-      Example 2
+    <div class="row mb-3">
+      <div class="col">
+        <button type="button" class="btn btn-primary" @click="showModal1 = true">Open a modal 1 (with modelValue)</button>
+        <button type="button" class="btn btn-primary ml-2" @click="showModal2 = true">Open a modal 2 (with v-model)</button>
+      </div>
     </div>
+
+    <Modal :model-value="showModal1" title="My first modal" @update:modelValue="showModal1 = false">
+      <p>Modal 1 content goes here...</p>
+    </Modal>
+
+    <Modal v-model="showModal2" title="My first modal">
+      <p>Modal 2 content goes here...</p>
+    </Modal>
   </div>
 </template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      showModal1: false,
+      showModal2: false
+    }
+  }
+}
+</script>
