e515a6325c57c6af599c1ad16ffae037cebf6135
Mirek Długosz <mirek@mirekdlugosz.com>
2022-04-03 11:37:17 +0200

    Update postcss.config.js
    - purgecss renamed `whitelist` to `safelist` and collapsed two options
      into one (see their issues #428 and #439)
    - current regexp was missing `vm` class

--- postcss.config.js
+++ postcss.config.js
@@ -10,13 +10,12 @@ module.exports = {
           const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
           return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
         },
-        whitelist: [],
-        whitelistPatterns: [
+        safelist: [
           /-(leave|enter|appear)(|-(to|from|active))$/,
           /^(?!(|.*?:)cursor-move).+-move$/,
           /^router-link(|-exact)-active$/,
           /data-v-.*/,
-          /vm-.*/
+          /^vm(-.*)?$/
         ]
       })
   ]
